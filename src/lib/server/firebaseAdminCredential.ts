cc758ef83312b0331b9d64fd6091294a5f53deb8
Adrian Moya <moyaa1022@gmail.com>
2023-03-05 03:16:22 +0000

    fix(vars): clean key var for netlify

--- src/lib/server/firebaseAdminCredential.ts
+++ src/lib/server/firebaseAdminCredential.ts
@@ -1,9 +1,11 @@
 import { FIREBASE_SERVICE_PRIVATE_KEY, FIREBASE_SERVICE_PRIVATE_KEY_ID } from '$env/static/private';
+console.log(`Firebase key: ${FIREBASE_SERVICE_PRIVATE_KEY.length}`);
 export default {
     type: "service_account",
     projectId: "csma-blend",
     privateKeyId: FIREBASE_SERVICE_PRIVATE_KEY_ID,
-    privateKey: FIREBASE_SERVICE_PRIVATE_KEY,
+    // The split/join thing is bevause Netlify will escape the newline characters in environment variables, which makes the key unreadable to Firebase
+    privateKey: FIREBASE_SERVICE_PRIVATE_KEY.split("\\n").join("\n"),
     clientEmail: "firebase-adminsdk-rchzc@csma-blend.iam.gserviceaccount.com",
     clientId: "104002520848926491273",
     authUri: "https://accounts.google.com/o/oauth2/auth",
