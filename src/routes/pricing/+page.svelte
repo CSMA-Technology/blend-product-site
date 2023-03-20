2bbea10ee0e6f848241a45801308197337b1fa22
Stephanie Cruz <exscruzme@gmail.com>
2023-03-20 13:39:26 -0500

    feat(navbar): added the navbar

---
+++ src/routes/pricing/+page.svelte
@@ -0,0 +1,12 @@
+<h1>Pricing</h1>
+
+<div class="content">
+    <p>Pricing Table goes here</p>
+</div>
+
+<style>
+    .content {
+        max-width: 75rem;
+        padding-bottom: 30rem;
+    }
+</style>
