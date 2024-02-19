8f5190957e35a224227f7527d89d641fd6352f65
Stephanie Cruz <exscruzme@gmail.com>
2024-02-19 11:34:20 -0500

    fix(PR): refactored query params to omitOrgDecks and orgId

--- src/routes/api/decks/user/+server.ts
+++ src/routes/api/decks/user/+server.ts
@@ -4,21 +4,18 @@ import type { RequestHandler } from './$types';
 
 export const GET = (async (event) => {
   const { uid } = await authenticate(event);
-  const viewParam = event.url.searchParams.get('view');
+  const omitOrgDecks = event.url.searchParams.get('omitOrgDecks') ?? false;
   const orgIdParam = event.url.searchParams.get('orgId');
-  if (orgIdParam && !viewParam) return new Response('If orgId is provided, view must be set to org', {status: 400, headers: [['Access-Control-Allow-Origin', '*']]});
-  switch (viewParam) {
-    case 'user': {
-      const userDecks = (await readPath<Database.Decks.User>(`/decks/user/${uid}`)) || {};
+  if (omitOrgDecks && orgIdParam) return new Response('Cannot specify both omitOrgDecks and orgId', {status: 400, headers: [['Access-Control-Allow-Origin', '*']]});
+  if (omitOrgDecks) {
+    const userDecks = (await readPath<Database.Decks.User>(`/decks/user/${uid}`)) || {};
       const deckArray = Object.entries(userDecks).map(([key, val]) => val);
       return json(deckArray, {
         headers: [['Access-Control-Allow-Origin', '*']],
       });
-    }
-    case 'org': {
-      const organizationIds = await getUserOrganizations(uid);
-      if (orgIdParam) {
-        if (!organizationIds.includes(orgIdParam)) return new Response('User is not a member of this organization', {status: 403, headers: [['Access-Control-Allow-Origin', '*']]});
+  } else if (orgIdParam) {
+    const organizationIds = await getUserOrganizations(uid);
+    if (!organizationIds.includes(orgIdParam)) return new Response('User is not a member of this organization', {status: 403, headers: [['Access-Control-Allow-Origin', '*']]});
         const orgDecks = (await getOrganizationDecks(orgIdParam)) ?? {};
         const organizationInfo = await getOrganizationInfo(orgIdParam);
         const deckArray = Object.values(orgDecks).map(({ deck }) => ({
@@ -32,29 +29,8 @@ export const GET = (async (event) => {
         return json(deckArray, {
           headers: [['Access-Control-Allow-Origin', '*']],
         });
-      } else {
-        const organizationDeckArray = (
-          await Promise.all(
-            organizationIds.map(async (orgId) => {
-              const decks = (await getOrganizationDecks(orgId)) ?? {};
-              const orgInfo = await getOrganizationInfo(orgId);
-              return Object.values(decks).map(({ deck }) => ({
-                ...deck,
-                orgSource: {
-                  orgName: orgInfo?.name,
-                  orgId,
-                },
-              }));
-            }),
-          )
-        ).flat();
-        return json(organizationDeckArray, {
-          headers: [['Access-Control-Allow-Origin', '*']],
-        });
-      }
-    }
-    default: {
-      const userDecks = (await readPath<Database.Decks.User>(`/decks/user/${uid}`)) || {};
+  } else {
+    const userDecks = (await readPath<Database.Decks.User>(`/decks/user/${uid}`)) || {};
       const userDeckArray = Object.entries(userDecks).map(([key, val]) => val);
       const positionOffset = Math.max(...userDeckArray.map(({ position }) => position), 0) + 2; // +2 because legacy position can be -1, and we want the new position to be strictly greater TODO: refactor this once we get rid of the legacy magic numbers in positioning
       const organizationIds = await getUserOrganizations(uid);
@@ -78,7 +54,6 @@ export const GET = (async (event) => {
       return json([...userDeckArray, ...organizationDeckArray], {
         headers: [['Access-Control-Allow-Origin', '*']],
       });
-    }
   }
 }) satisfies RequestHandler;
 
