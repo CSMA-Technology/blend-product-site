3b3fa934deeecc8c5998854b11d5d190ff8b3567
Stephanie Cruz <exscruzme@gmail.com>
2024-03-17 15:35:26 -0500

    feat(maintenance-preloaded-decks): endpoint to copy preloaded decks for users who have playlists linked to them
    added more comments
    
    formatting

---
+++ src/routes/api/maintenance/copyPreloadedDecks/+server.ts
@@ -0,0 +1,65 @@
+//@ts-nocheck
+import type { RequestHandler } from './$types';
+import { db, readPath, writePath } from '$lib/server/firebaseUtils';
+
+const createDeckCopy = async (deckId: number, userId: string) => {
+  // Generate new reference ID
+  const newRefId = Math.floor(Math.random() * 4294967295);
+
+  // Fetch linked deck
+  const linkedDeck = await readPath<Database.Decks.Preloaded>(`/decks/preloaded/${deckId}`);
+  if (linkedDeck) {
+    // Create a copy of the linked deck with updated fields
+    const timestamp = new Date().toISOString().split('.')[0];
+    const deckCopy = { ...linkedDeck };
+    deckCopy.created_ts = timestamp;
+    deckCopy.modified_ts = timestamp;
+    deckCopy.position = -1;
+    deckCopy.refId = newRefId;
+    deckCopy.is_editable = true;
+
+    // Save the copy to the user's decks
+    await writePath<Database.Decks.User>(`/decks/user/${userId}/${newRefId}`, deckCopy);
+  }
+};
+
+export const POST: RequestHandler = async (event) => {
+  const playlists = (await db.ref('/playlists/user').get()).val();
+
+  const counts: { [userId: string]: { [id: string]: number } } = {};
+
+  if (playlists) {
+    Object.keys(playlists).forEach(async (userId) => {
+      counts[userId] = { '113822357': 0, '541852177': 0 }; // Initialize counts for each user
+      const userPlaylists = playlists[userId];
+
+      Object.keys(userPlaylists).forEach(async (playlistId) => {
+        const playlist = userPlaylists[playlistId] as PlaylistData;
+        if (playlist.linked_deck_id === 113822357 || playlist.linked_deck_id === 541852177) {
+          counts[userId][playlist.linked_deck_id]++;
+        }
+      });
+    });
+  }
+
+  Object.keys(counts).forEach((userId) => {
+    const count113822357 = counts[userId]['113822357'];
+    const count541852177 = counts[userId]['541852177'];
+
+    if (count113822357 > 0 || count541852177 > 0) {
+      console.log('=====================================');
+      console.log(`For User ID: ${userId}`);
+      if (count113822357 > 0) {
+        console.log('Found this many playlists linked to Blends and Advanced Vowels:', count113822357);
+        console.log('Creating copy of Blends and Advanced Vowels for this user.');
+        createDeckCopy(113822357, userId);
+      }
+      if (count541852177 > 0) {
+        console.log('Found this many playlists linked to CVC and Magic E:', count541852177);
+        console.log('Creating copy of CVC and Magic E for this user.');
+        createDeckCopy(541852177, userId);
+      }
+    }
+  });
+  return new Response();
+};
