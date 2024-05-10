ee7359c010fbbec55253b8063b8077cfc4dbf037
Stephanie Cruz <exscruzme@gmail.com>
2024-05-10 16:18:46 -0500

    feat(preloaded-playlists): added api endpoint for preloaded playlists

---
+++ src/routes/api/playlists/preloaded/+server.ts
@@ -0,0 +1,22 @@
+import { readPath, weaklyAuthenticate } from '$lib/server/firebaseUtils';
+import { json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const GET = (async (request) => {
+  const user = await weaklyAuthenticate(request);
+  const playlists = (await readPath<Database.Playlists.Preloaded>('/playlists/preloaded')) || {};
+  const playlistArray = Object.entries(playlists).map(([key, val]) => val);
+  return json(playlistArray, {
+    headers: [['Access-Control-Allow-Origin', '*']],
+  });
+}) satisfies RequestHandler;
+
+export const OPTIONS = (() => {
+  return new Response(null, {
+    headers: [
+      ['Access-Control-Allow-Origin', '*'],
+      ['Access-Control-Allow-Headers', '*'],
+      ['Access-Control-Allow-Methods', 'GET'],
+    ],
+  });
+}) satisfies RequestHandler;
