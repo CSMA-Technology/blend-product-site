8e27f8e71cfdcb8bc174abee2716083e94050ad1
Adrian Moya <adrian@csma.tehcnology>
2024-05-15 19:59:26 +0000

    inline org detail retrieval

--- src/routes/api/user/+server.ts
+++ src/routes/api/user/+server.ts
@@ -1,5 +1,5 @@
 import { authenticate, getOrganizationInfo, getUserData, getUserOrganizations } from '$lib/server/firebaseUtils';
-import { isOrganizationMember, isStripeCustomerSubscribedToBlendPro, isSubscribedToBlendPro } from '$lib/server/subscriptionUtils';
+import { isSubscribedToBlendPro } from '$lib/server/subscriptionUtils';
 import type { RequestHandler } from './$types';
 
 export const GET = (async (event) => {
@@ -7,30 +7,28 @@ export const GET = (async (event) => {
   const { uid } = await authenticate(event);
   const authenticateTime = Date.now();
   console.log(`Authenticated in ${authenticateTime - startTime}ms`);
-  const [firebaseUserData, isProSubscriber, userOrganizations] = await Promise.all([
+  const [firebaseUserData, isProSubscriber, organizationInfo] = await Promise.all([
     getUserData(uid),
     isSubscribedToBlendPro(uid),
-    getUserOrganizations(uid),
+    getUserOrganizations(uid).then((orgs) =>
+      Promise.all(
+        orgs.map(async (orgId) => {
+          const orgInfo = await getOrganizationInfo(orgId);
+          return {
+            orgName: orgInfo?.name,
+            orgId,
+          };
+        }),
+      ),
+    ),
   ]);
   const userDataTime = Date.now();
   console.log(`Got user data in ${userDataTime - authenticateTime}ms`);
-  const organizationInfo = await Promise.all(
-    userOrganizations.map(async (orgId) => {
-      const orgInfo = await getOrganizationInfo(orgId);
-      return {
-        orgName: orgInfo?.name,
-        orgId,
-      };
-    }),
-  );
-  const orgInfoTime = Date.now();
-  console.log(`Got organization info in ${orgInfoTime - userDataTime}ms`);
   const userData = {
     ...firebaseUserData,
-    isSubscribedToBlendPro: isProSubscriber || (await isOrganizationMember(uid)),
+    isSubscribedToBlendPro: isProSubscriber || organizationInfo.length > 0,
     organizationInfo,
   };
-  console.log(`Validated Pro membership in ${Date.now() - orgInfoTime}ms`);
   console.log(`Total time: ${Date.now() - startTime}ms`);
   return new Response(JSON.stringify(userData, null, 2));
 }) satisfies RequestHandler;
