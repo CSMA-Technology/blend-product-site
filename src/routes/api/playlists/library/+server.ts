0c33c5bf04e9bdcf9ead20543f65e90ea2455c44
Adrian Moya <adrian@csma.tehcnology>
2024-05-23 20:27:33 +0000

    test 3

--- src/routes/api/playlists/library/+server.ts
+++ src/routes/api/playlists/library/+server.ts
@@ -1,19 +1,11 @@
-import { readPath } from '$lib/server/firebaseUtils';
+import { readPathWithCache } from '$lib/server/firebaseUtils';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import { filterAttributes, transformPlaylistForClient } from '$lib/utils';
 
-let playlistsPromise = readPath<Database.Playlists.Library>('/playlists/library');
-const refreshPlaylistsPromise = () => {
-  console.log('Refreshing playlists');
-  playlistsPromise = readPath<Database.Playlists.Library>('/playlists/library');
-};
-setInterval(refreshPlaylistsPromise, 30000);
-
 export const GET = (async (request) => {
   const attributes = request.url.searchParams.get('attributes')?.split(',') ?? [];
-  console.log('Awaiting playlists');
-  const playlists = (await playlistsPromise) || {};
+  const playlists = (await readPathWithCache<Database.Playlists.Library>('/playlists/library')) || {};
   const playlistArray = Object.values(playlists).map((playlist) => ({
     ...playlist,
     words: transformPlaylistForClient(playlist) ?? [],
