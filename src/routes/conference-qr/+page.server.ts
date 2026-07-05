03374342581747082e9f291a221c6438109ae62f
Adrian Moya <adrian@csma.technology>
2026-07-05 21:09:01 +0000

    update promo codes

--- src/routes/conference-qr/+page.server.ts
+++ src/routes/conference-qr/+page.server.ts
@@ -1,6 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 
-const conferenceRedirect = 'http://blendreading.com/account?action=upgrade&subscriptionType=yearly&promoCode=TRL2025&skipTrial';
+const conferenceRedirect = 'http://blendreading.com/account?action=upgrade&subscriptionType=yearly&promoCode=ELEVATEVEGAS&skipTrial';
 
 export const load = async () => {
   throw redirect(302, conferenceRedirect);
